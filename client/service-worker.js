b703d6d2e4afde096666a66536057fb2a1017bbb
Christina Delancy <cdelancy11@yahoo.com>
2022-01-24 17:46:39 -0500

    commit before pull request

--- client/service-worker.js
+++ client/service-worker.js
@@ -10,7 +10,7 @@
 // To learn more about the benefits of this model and instructions on how to
 // opt-in, read http://bit.ly/CRA-PWA
 
-import 'dotenv/config'
+// import 'dotenv/config'
 
 
 const isLocalhost = Boolean(
