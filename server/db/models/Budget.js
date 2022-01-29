c9c7d44d8ef056c3858126d99540781b3bb0a99f
addislw <addislw@gmail.com>
2022-01-29 15:47:39 -0500

    Start and End date attributes set for Document

--- server/db/models/Budget.js
+++ server/db/models/Budget.js
@@ -12,9 +12,49 @@ const BudgetSchema = new Schema({
   },
   category: {
     type: String,
+    required: true,
     unique: 1
+  },
+  startDate: {
+    type: Date,
+    required: true,
+    get: formatDate
+  },
+  endDate: {
+    type: Date,
+    required: true,
+    get: formatDate
   }
 })
 
-const Budget = BudgetSchema
+function formatDate(date) {
+  return date.toISOString().split('T')[0];
+}
+
+
+BudgetSchema.statics.doStuff = async function ({category, amount}) {
+  const budget = await this.find({category: category, amount: amount});
+
+  return budget;
+
+  // const budget = await this.model('Budget').find({})
+  // const budget = await this.where('name', new RegExp(name, 'i')).exec(cb)
+};
+
+
+BudgetSchema.pre('validate', async function(next) {
+  const info = {category: this.category, amount: this.amount};
+  /*
+  const testVar = BudgetSchema.doStuff(info)
+  if(testVar.amount) {
+    const err = new Error('something went wrong');
+    next(err)
+  }
+  */
+
+});
+
+
+// const Budget = mongoose.model("budgets", BudgetSchema);
+const Budget = BudgetSchema;
 module.exports = Budget
