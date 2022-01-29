5130fb0c432ee558fc47f4b7718a744f475862d8
addislw <addislw@gmail.com>
2022-01-29 15:50:39 -0500

    Component refactor: placed in react-bootstrap Card

--- client/components/BudgetItem.js
+++ client/components/BudgetItem.js
@@ -1,3 +1,53 @@
+import React, { Component } from "react";
+import { Chart as ChartJS, ArcElement, Tooltip, Legend, Title } from 'chart.js';
+import moment from 'moment';
+ChartJS.register(ArcElement, Tooltip, Legend, Title);
+import BudgetGraph from "./BudgetGraph";
+import { Card } from 'react-bootstrap';
+
+const BudgetItem = (props) => {
+  const { budget, transactions } = props;
+
+  let dateRange = (date) => {
+    return moment(date).isBetween(budget.startDate, budget.endDate)
+  }
+
+  let hasBudget = Object.keys(budget).length
+  let totalSpent;
+  let amountRemaining;
+
+  if (hasBudget) {
+    totalSpent = transactions
+      .filter(transaction => transaction.category === budget.category && dateRange(transaction.date))
+      .map(transaction => transaction.amount)
+      .reduce((acc, cur) => {
+        return acc += cur;
+      }, 0)
+    const amount = budget.amount - totalSpent;
+    amountRemaining = amount > 0 ? amount : 0;
+  }
+
+  return (
+      <Card className="budget-item">
+        <Card.Body>
+          <Card.Title>
+            <h6>Budget: {budget.category}</h6>
+          </Card.Title>
+          <Card.Text>
+            <h6>Amount: {budget.amount}</h6>
+            <h6>Date: {moment(budget.startDate).format('M/d/YYYY')} - {moment(budget.endDate).format('M/d/YYYY')}</h6>
+            <BudgetGraph budget={budget} totalSpent={totalSpent} amountRemaining={amountRemaining} />
+            <h6>Spent: {totalSpent} </h6>
+            <h6>Remaining: {amountRemaining > 0 ? amountRemaining : 'Over Budget!'} </h6>
+          </Card.Text>
+        </Card.Body>
+      </Card>
+  )
+}
+
+export default BudgetItem;
+
+
 /*
 import React from "react";
 import { Chart as ChartJS, ArcElement, Tooltip, Legend, Title } from 'chart.js';
@@ -88,56 +138,3 @@ const BudgetItem = (props) => {
 
 export default BudgetItem;
 */
-
-
-import React, { Component } from "react";
-import { connect } from "react-redux";
-import { addBudget } from "../store/plaid"
-import { Chart as ChartJS, ArcElement, Tooltip, Legend, Title } from 'chart.js';
-import { Doughnut } from 'react-chartjs-2';
-import moment from 'moment';
-ChartJS.register(ArcElement, Tooltip, Legend, Title);
-import BudgetGraph from "./BudgetGraph";
-import { Card, Button } from 'react-bootstrap';
-
-const BudgetItem = (props) => {
-  const { budget, transactions } = props;
-
-  let dateRange = (date) => {
-    return moment(date).isBetween(budget.startDate, budget.endDate)
-  }
-
-  let hasBudget = Object.keys(budget).length
-  let totalSpent;
-  let amountRemaining;
-
-  if (hasBudget) {
-    totalSpent = transactions
-      .filter(transaction => transaction.category === budget.category && dateRange(transaction.date))
-      .map(transaction => transaction.amount)
-      .reduce((acc, cur) => {
-        return acc += cur;
-      }, 0)
-    const amount = budget.amount - totalSpent;
-    amountRemaining = amount > 0 ? amount : 0;
-  }
-
-  return (
-      <Card className="budget-item">
-        <Card.Body>
-          <Card.Title>
-            <h6>Budget: {budget.category}</h6>
-          </Card.Title>
-          <Card.Text>
-            <h6>Amount: {budget.amount}</h6>
-            <h6>Date: {moment(budget.startDate).format('M/d/YYYY')} - {moment(budget.endDate).format('M/d/YYYY')}</h6>
-            <BudgetGraph budget={budget} totalSpent={totalSpent} amountRemaining={amountRemaining} />
-            <h6>Spent: {totalSpent} </h6>
-            <h6>Remaining: {amountRemaining > 0 ? amountRemaining : 'Over Budget!'} </h6>
-          </Card.Text>
-        </Card.Body>
-      </Card>
-  )
-}
-
-export default BudgetItem;
